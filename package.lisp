53a423f007f3318e3ced0d57c67d1b79d4d9896c
Elias Mårtenson <lokedhs@gmail.com>
2014-10-15 21:41:12 +0800

    Make sure the correct functions are exported

--- package.lisp
+++ package.lisp
@@ -1,9 +1,7 @@
-;;;; package.lisp
-
 (defpackage #:cl-markup
   (:use #:cl)
   (:export
    #:render-markup-to-stream
    #:render-markup-to-string
-   ))
-
+   #:markup-string
+   #:markup-paragraphs))
